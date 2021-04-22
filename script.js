9d7437afa5d138243b6c3e98fb34c8705abb4808
NoeFabris <noe@elysresidence.com>
2021-04-22 16:06:22 +0200

    Es.1

---
+++ script.js
@@ -0,0 +1,16 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+
+var studente = {
+    'nome' : 'Noè',
+    'cognome' : 'Fabris',
+    'eta' : '23'
+}
+for (var key in studente) {
+    console.log(key + (' : ') + studente[key])
+}
+
+
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
